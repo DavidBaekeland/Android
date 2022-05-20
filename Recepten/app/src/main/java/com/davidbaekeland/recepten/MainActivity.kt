1a3c081ea83aad0d56ca276a8097b61e91da93cf
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2022-05-20 09:49:55 +0200

    init project + change nav

---
+++ Recepten/app/src/main/java/com/davidbaekeland/recepten/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.davidbaekeland.recepten
+
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.davidbaekeland.recepten.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+    }
+
+    fun test() {
+        val textView = findViewById<TextView>(R.id.txt_test)
+        // ...
+
+        // Instantiate the RequestQueue.
+                val queue = Volley.newRequestQueue(this)
+                val url = "https://www.google.com"
+
+        // Request a string response from the provided URL.
+                val stringRequest = StringRequest(
+                    Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        // Display the first 500 characters of the response string.
+                        textView.text = "Response is: ${response.substring(0, 500)}"
+                    },
+                    Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+                queue.add(stringRequest)
+    }
+}
