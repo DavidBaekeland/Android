158081350586597d9dee394b6572d6b44157f518
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2022-05-28 17:50:21 +0200

    add data -> firebase

--- Recepten/app/src/main/java/com/davidbaekeland/recepten/models/Recept.kt
+++ Recepten/app/src/main/java/com/davidbaekeland/recepten/models/Recept.kt
@@ -1,4 +1,5 @@
 package com.davidbaekeland.recepten.models
 
-data class Recept(var id: Int, val image: String, val title: String) {
+// https://stackoverflow.com/questions/38802269/firebase-user-is-missing-a-constructor-with-no-arguments
+data class Recept(var id: Int = 0, val image: String = "", val title: String = "") {
 }
--- Recepten/app/src/main/java/com/davidbaekeland/recepten/models/Repository.kt
+++ Recepten/app/src/main/java/com/davidbaekeland/recepten/models/Repository.kt
@@ -1,8 +1,12 @@
 package com.davidbaekeland.recepten.models
 
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.davidbaekeland.recepten.ReceptenAdapter
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.toObject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,10 +14,12 @@ import kotlinx.coroutines.flow.flow
 class Repository {
 
     var test: Flow<MutableList<Recept>> = flow { data() }
+    var db = FirebaseFirestore.getInstance()
+    var check: Boolean = false
+
 
     fun data(): MutableList<Recept> {
         // Code afkomstig van lessen en FireStore
-        var db = FirebaseFirestore.getInstance()
         var receptenLijst: MutableList<Recept> = mutableListOf()
 
         val docRef = db.collection("italiaans")
@@ -34,5 +40,10 @@ class Repository {
             }
 
         return receptenLijst
+    }
+
+    fun addData(recept: Recept) {
+        db.collection("recepten").document(recept.id.toString())
+            .set(recept, SetOptions.merge())
     }
 }
