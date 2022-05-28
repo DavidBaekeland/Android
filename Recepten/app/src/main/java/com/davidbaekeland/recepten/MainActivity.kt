bc511c586a95c5c7148e0420a8aae674b00212e9
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2022-05-28 17:32:57 +0200

    feature (nav): added navigation in gradle

--- Recepten/app/src/main/java/com/davidbaekeland/recepten/MainActivity.kt
+++ Recepten/app/src/main/java/com/davidbaekeland/recepten/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -37,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
